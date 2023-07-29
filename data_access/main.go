d9b7cbf6c7001c1fcd6b1ac4783cd176351d51b8
Melo <meluszko@gmail.com>
2023-07-29 19:02:00 +0200

    add functions that handle basic db queries

--- data_access/main.go
+++ data_access/main.go
@@ -10,6 +10,13 @@ import (
 
 var db *sql.DB
 
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
 func main() {
 	// Capture connection properties.
 	config := mysql.Config{
@@ -32,4 +39,81 @@ func main() {
 	}
 	fmt.Println("Connected! o.O")
 
+	// albums, err := albumsByArtist("John Coltrane")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Albums found: %v\n", albums)
+
+	album, err := albumsByID(5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", album)
+
+	// albumID, err := addAlbum(Album{
+	// 	Title:  "The Modern Sound of Betty Carter",
+	// 	Artist: "Betty Carter",
+	// 	Price:  49.99,
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("ID of added album: %v\n", albumID)
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	var albums []Album
+
+	rows, err := db.Query(
+		"SELECT * FROM album WHERE artist = ?", name,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArist %q: %v", name, err)
+	}
+
+	defer rows.Close()
+
+	// Loop through rows, using Scan to assign columndata to struct fields
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
+}
+
+func albumsByID(id int64) (Album, error) {
+	var album Album
+
+	row := db.QueryRow(
+		"SELECT * FROM album WHERE id = ?", id,
+	)
+	if err := row.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return album, fmt.Errorf("albumsByID %d: no such album", id)
+		}
+		return album, fmt.Errorf("albumsByID %d: %v", id, err)
+	}
+	return album, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(album Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", album.Title, album.Artist, album.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAalbym: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
 }
